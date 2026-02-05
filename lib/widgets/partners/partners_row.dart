feaa7b57abe8dda1686fb89a825a61b9c48200e8
Vinícius Bornhofen <vinicius.bornhofen47@gmail.com>
2026-02-05 14:07:02 -0300

    pequeno ajuste na parte de parceiros

--- lib/widgets/partners/partners_row.dart
+++ lib/widgets/partners/partners_row.dart
@@ -7,23 +7,25 @@ class PartnersRow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/images/redragon.png', height: 50),
-
-          const SizedBox(width: 8),
-          const Text('—', style: TextStyle(fontSize: 24, color: Colors.grey)),
-          const SizedBox(width: 8),
-
-          Image.asset('assets/images/avon.png', height: 50),
-
-          const SizedBox(width: 8),
-          const Text('—', style: TextStyle(fontSize: 24, color: Colors.grey)),
-          const SizedBox(width: 8),
-
-          Image.asset('assets/images/playboy.png', height: 50),
-        ],
+      child: Expanded(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/redragon.png', height: 50),
+        
+            const SizedBox(width: 4),
+            const Text('—', style: TextStyle(fontSize: 24, color: Colors.grey)),
+            const SizedBox(width: 4),
+        
+            Image.asset('assets/images/avon.png', height: 50),
+        
+            const SizedBox(width: 4),
+            const Text('—', style: TextStyle(fontSize: 24, color: Colors.grey)),
+            const SizedBox(width: 4),
+        
+            Image.asset('assets/images/playboy.png', height: 50),
+          ],
+        ),
       ),
     );
   }
